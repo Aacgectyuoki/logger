db2c06ee42be47829265d3a41f73942023613636
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2021-04-05 14:52:24 -0400

    Update writer.js

--- writer.js
+++ writer.js
@@ -1,27 +1,35 @@
 let fs = require("fs");
 let obj = require("readline-sync");
+
+// ask user for number of inputs
+var entries = obj.question("Enter the number of entries: ");
+console.log(entries);
+debugger;
+
+let emp = new Array();
+
 // ask user for inputs
-let id = obj.question("Enter your ID #: ");
-console.log("ID: "+id);
-debugger;
+for (var i=0; i<parseInt(entries); i++){
 
-let name = obj.question("Enter your full name: ");
-console.log("name: "+name);
-debugger;
+    let id = obj.question("Enter your ID #: ");
+    console.log("ID: "+id);
+    debugger;
 
-let email = obj.question("Enter your email address: ");
-console.log("email address: "+email);
-debugger;
+    let name = obj.question("Enter your full name: ");
+    console.log("name: "+name);
+    debugger;
 
-// store inputs into JSON format
-let empObj = {"id":id,"name":name,"email":email, "time entry was created": new Date().toISOString()};
+    let email = obj.question("Enter your email address: ");
+    console.log("email address: "+email);
+    debugger;
 
-// convert object to string 
-var empString = JSON.stringify(empObj);
+    // store inputs into JSON format
+    let emp1 = {"id":id, "name":name,"salary":email, "time": new Date().toISOString()};
+    emp.push(emp1);
+    debugger;
+}
 
-fs.writeFile("records.json",empString,{flag:"a"},(err)=> {
-    if(!err){
-        console.log("Success!")
-        debugger;
-    }
-})
+// convert array object to string 
+let jsonData = JSON.stringify(emp);
+fs.writeFileSync("records.json", jsonData);
+console.log('Success!');
