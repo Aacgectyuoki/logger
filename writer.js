2364eafaf9c667b684a48ba9c1930fbc29837596
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2021-04-05 13:37:17 -0400

    Add files via upload

---
+++ writer.js
@@ -0,0 +1,28 @@
+let fs = require("fs");
+let obj = require("readline-sync");
+
+// ask user for inputs
+let id = obj.question("Enter your ID #: ");
+console.log("ID: "+id);
+debugger;
+
+let name = obj.question("Enter your full name: ");
+console.log("name: "+name);
+debugger;
+
+let email = obj.question("Enter your email address: ");
+console.log("email address: "+email);
+debugger;
+
+// store inputs into JSON format
+let empObj = {"id":id,"name":name,"email":email, "time entry was created": new Date().toISOString()};
+
+// convert object to string 
+var empString = JSON.stringify(empObj);
+
+fs.writeFile("records.json",empString,{flag:"a"},(err)=> {
+    if(!err){
+        console.log("Success!")
+        debugger;
+    }
+})
